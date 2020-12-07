1a34738501f8f2690b04f590945817e91bbce167
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-07 20:31:45 +0900

    Add startLoading Method

--- Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.example.gno.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.gno.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        startLoading()
+    }
+
+    // 앱 실행 후 2초 뒤에 MainActivity로 전환
+    private fun startLoading() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) // MainActivity로 화면 전환
+            finish() // 꼭 finish()를 해줘야 함
+        }, 2000) // 2초 후
     }
 }
