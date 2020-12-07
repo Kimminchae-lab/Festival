ce6b53d3b81613cb6af7465ef6ccb13aff0a0e9e
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-07 19:39:56 +0900

    Android

---
+++ Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gno.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gno.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
