c4a733c798962309f91644eabde8a96d0f46dadf
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-07 23:50:40 +0900

    로그인이 되어있는지 아닌지 검사

--- Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.Window
 import androidx.appcompat.app.ActionBar
+import androidx.core.view.isVisible
 import com.example.gno.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +28,13 @@ class SplashActivity : AppCompatActivity() {
 
     // 앱 실행 후 2초 뒤에 MainActivity로 전환
     private fun startLoading() {
+        // 로그인이 되어있는 상태라면
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent) // MainActivity로 화면 전환
             finish() // 꼭 finish()를 해줘야 함
         }, 2000) // 2초 후
+        // 로그인이 되지 않았을 시
+        loginButton.isVisible = true
     }
 }
