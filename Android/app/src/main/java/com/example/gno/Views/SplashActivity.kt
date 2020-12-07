a544e099f8ee484d0321c48d8b6ebeb696e69e83
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-07 20:25:09 +0900

    Splash

--- Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.gno.Views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gno.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
