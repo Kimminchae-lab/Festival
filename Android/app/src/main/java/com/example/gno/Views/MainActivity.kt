78e119e15b15dc6c49949660ba9f43ace726e08c
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-08 23:45:39 +0900

    ToolBar

--- Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.gno.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gno.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolBar)
     }
 }
