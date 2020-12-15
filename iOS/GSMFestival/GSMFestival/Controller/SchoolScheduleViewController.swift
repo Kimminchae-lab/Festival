58ff5b8eba6d2649c9f1c5f6c42987e02e364340
jo030225 <whwngur123@naver.com>
2020-12-15 17:10:07 +0900

    FSCalendar 수정

--- iOS/GSMFestival/GSMFestival/Controller/SchoolScheduleViewController.swift
+++ iOS/GSMFestival/GSMFestival/Controller/SchoolScheduleViewController.swift
@@ -12,40 +12,34 @@ class SchoolScheduleViewController: UIViewController {
     
     let dateFormatter = DateFormatter()
     
+    @IBOutlet weak var calendarView: FSCalendar! {
+        didSet{
+            
+            calendarView.delegate = self
+            calendarView.dataSource = self
+            
+            calendarView.translatesAutoresizingMaskIntoConstraints = false
+            calendarView.headerHeight = 50
+            calendarView.appearance.headerMinimumDissolvedAlpha = 0.0
+            calendarView.appearance.headerDateFormat = "YYYY년 M월"
+            calendarView.appearance.headerTitleColor = .black
+            calendarView.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
+            calendarView.locale = Locale(identifier: "ko_KR")
+            calendarView.backgroundColor = UIColor(red: 241/255, green: 249/255, blue: 255/255, alpha: 1)
+            calendarView.appearance.selectionColor = UIColor(red: 38/255, green: 153/255, blue: 251/255, alpha: 1)
+            calendarView.appearance.todayColor = UIColor(red: 188/255, green: 224/255, blue: 253/255, alpha: 1)
+            calendarView.appearance.headerMinimumDissolvedAlpha = 0.0
+           
+        }
+    }
     
-    var calendarView: FSCalendar = {
-        let calendarview = FSCalendar()
+   
         
-        calendarview.translatesAutoresizingMaskIntoConstraints = false
-        calendarview.headerHeight = 50
-        calendarview.appearance.headerMinimumDissolvedAlpha = 0.0
-        calendarview.appearance.headerDateFormat = "YYYY년 M월"
-        calendarview.appearance.headerTitleColor = .black
-        calendarview.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
-        calendarview.locale = Locale(identifier: "ko_KR")
-        calendarview.backgroundColor = UIColor(red: 241/255, green: 249/255, blue: 255/255, alpha: 1)
-        calendarview.appearance.selectionColor = UIColor(red: 38/255, green: 153/255, blue: 251/255, alpha: 1)
-        calendarview.appearance.todayColor = UIColor(red: 188/255, green: 224/255, blue: 253/255, alpha: 1)
-        calendarview.appearance.headerMinimumDissolvedAlpha = 0.0
-       
-        return calendarview
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        calendarView.delegate = self
-        calendarView.dataSource = self
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        view.addSubview(calendarView)
-        
-        calendarView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 140).isActive = true
-        calendarView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
-        calendarView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
-        calendarView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
         
     }
     
