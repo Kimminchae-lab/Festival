7e74eb03f7950d2b76d693e12ec0086c8ad7ac1c
jo030225 <whwngur123@naver.com>
2020-12-21 17:54:57 +0900

    ClassVC tableView 생성 중

---
+++ iOS/GSMFestival/GSMFestival/ClassTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ClassTableViewCell.swift
+//  GSMFestival
+//
+//  Created by 조주혁 on 2020/12/21.
+//
+
+import UIKit
+
+class ClassTableViewCell: UITableViewCell {
+
+   
+
+}
--- iOS/GSMFestival/GSMFestival/Controller/ClassViewController.swift
+++ iOS/GSMFestival/GSMFestival/Controller/ClassViewController.swift
@@ -11,7 +11,7 @@ class ClassViewController: UIViewController {
 
     var tableView: UITableView = {
         let tableview = UITableView()
-        
+        tableview.translatesAutoresizingMaskIntoConstraints = false
         return tableview
     }()
     
@@ -26,23 +26,27 @@ class ClassViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        view.addSubview(tableView)
         
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
     @objc func back() {
         dismiss(animated: true, completion: nil)
     }
-    
-    
-    
 }
 
 extension ClassViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
         return
     }
     
