2503b674dbe533b5096724f29711f1bb85b98055
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-01-01 23:54:43 +0900

    Update MainActivity.kt

--- Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.gno.Views
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gno.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,6 @@ class MainActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar?.hide()
 
+        toolBar.setBackgroundColor(Color.BLUE)
     }
 }
