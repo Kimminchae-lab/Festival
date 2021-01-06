a2bad333e4be2d7ec664313b8a9bfc290f168a57
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-01-06 22:10:47 +0900

    Update LoginActivity.kt

--- Android/app/src/main/java/com/example/gno/Views/LoginActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/LoginActivity.kt
@@ -1,12 +1,43 @@
 package com.example.gno.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import com.example.gno.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_Login.setOnClickListener { startLoading() }
     }
+
+    // 2초 후 화면 전환(LoginActivity -> MainActivity)
+    private fun startLoading() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+
+    // region 뒤로가기 두 번 누르면 종료
+    private var backPressedTime: Long = 0
+
+    override fun onBackPressed() {
+        // 2초내 다시 클릭하면 앱 종료
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            finish()
+            return
+        }
+
+        // 처음 클릭 메시지
+        Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        backPressedTime = System.currentTimeMillis()
+    }
+    // endregion
 }
