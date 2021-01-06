68cc44256d11ef37d0db98d55e784ed68119b719
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-01-06 22:10:49 +0900

    Update MainActivity.kt

--- Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
+++ Android/app/src/main/java/com/example/gno/Views/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gno.Views
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.gno.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,4 +26,20 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    // region 뒤로가기 두 번 누르면 종료
+    private var backPressedTime: Long = 0
+
+    override fun onBackPressed() {
+        // 2초내 다시 클릭하면 앱 종료
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            finish()
+            return
+        }
+
+        // 처음 클릭 메시지
+        Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        backPressedTime = System.currentTimeMillis()
+    }
+    // endregion
 }
